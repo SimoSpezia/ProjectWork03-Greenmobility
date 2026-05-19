8ee3297860696a32cbbb3b66dcf600e472f01eef
SpezialeSimone <SpezialeSimone@CLOUDJ-SPEZIALE>
2026-05-19 14:11:57 +0200

    Creazione dto e ruoli per l'autenticazione

--- GreenMobility-be/Program.cs
+++ GreenMobility-be/Program.cs
@@ -4,16 +4,17 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddSqlServer<GreenMobilityDbContext>(builder.Configuration.GetConnectionString("Default"));
 
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<GreenMobilityDbContext>()
+                .AddDefaultTokenProviders();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
