ea67201de43c6136d7b2f0f1feecd1693bebfebd
Fernando Palamandadige <palamandadige.fernando@itsincom.it>
2026-05-19 15:25:21 +0200

    Creazione DTO veicolo

---
+++ GreenMobility-be/Dto/VehicleCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenMobility_be.Dto
+{
+    public class VehicleCreateDto
+    {
+        public int VehicleTypeId { get; set; }
+        public int HubId { get; set; }
+        [MaxLength(50)]
+        public required string UIC { get; set; }
+    }
+}
---
+++ GreenMobility-be/Dto/VehicleUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using GreenMobility_be.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenMobility_be.Dto
+{
+    public class VehicleUpdateDto
+    {
+        public int? VehicleTypeId { get; set; }
+        public int? VehicleStatusId { get; set; }
+        public int? HubId { get; set; }
+        [MaxLength(50)]
+        public string? UIC { get; set; }
+        [Range(0, 100)]
+        public int? BatteryLevel { get; set; }
+    }
+}
