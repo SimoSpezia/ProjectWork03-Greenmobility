191b2db9bea04e1c46f6113e5c15111eb6a44ac8
Fernando Palamandadige <palamandadige.fernando@itsincom.it>
2026-05-20 00:56:02 +0200

    Creazione DTO utente

---
+++ GreenMobility-be/Dto/UserCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using GreenMobility_be.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenMobility_be.Dto
+{
+    public class UserCreateDto
+    {
+        [MaxLength(100)]
+        [Required]
+        public required string Name { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public required string Surname { get; set; }
+        [EmailAddress]
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        [Required]
+        public required string Role { get; set; }
+    }
+}
---
+++ GreenMobility-be/Dto/UserUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenMobility_be.Dto
+{
+    public class UserUpdateDto
+    {
+        [MaxLength(100)]
+        [Required]
+        public required string Name { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public required string Surname { get; set; }
+        [EmailAddress]
+        [Required]
+        public required string Email { get; set; }
+
+    }
+}
